87c7f2ae484e4675b19d4d300874c740d9b59211
Hari Raval <hraval@princeton.edu>
2020-04-02 00:55:16 -0400

    updating script and configuration object to handle thread saturation

--- configuration.py
+++ configuration.py
@@ -4,14 +4,19 @@
 # -----------------------------------------------------------------------
 
 
-# a configuration object represents the inputs provided to a
+# a Configuration object represents the parameters and settings used to generate an Amber test
 class Configuration(object):
 
-    # constructor of the Course Object
-    def __init__(self, timeout, workgroups, threads_per_workgroup):
+    # constructor of the Configuration object
+    def __init__(self, timeout, workgroups, threads_per_workgroup, saturation_level):
+        # timeout represents the time (in ms) for which the Amber test will run
         self._timeout = timeout
+        # number of workgroups to be used for the Amber test
         self._workgroups = workgroups
+        # number of threads per workgroup
         self._threads_per_workgroup = threads_per_workgroup
+        # type of saturation: 0 means no saturation, 1 means "round robin" saturation, 2 means "chunking" saturation
+        self._saturation_level = saturation_level
 
     # getter method to retrieve the timeout
     def get_timeout(self):
@@ -24,3 +29,7 @@ class Configuration(object):
     # getter method to retrieve the number of threads per workgroup
     def get_threads_per_workgroup(self):
         return self._threads_per_workgroup
+
+    # getter method to retrieve the type of saturation (value of 0, 1 or 2) of the Amber test
+    def get_saturation_level(self):
+        return self._saturation_level
